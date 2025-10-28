bc1b163886f2695d6bafcd90dd1778fd87cce075
PavloChechehov <paulch1991@gmail.com>
2025-10-28 15:30:49 +0100

    LRU Cache problem solution using the LinkedHashMap

--- src/main/java/com/paul/LRUCacheV2.java
+++ src/main/java/com/paul/LRUCacheV2.java
@@ -1,4 +1,36 @@
 package com.paul;
 
-public class LRUCacheV2 {
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Implementation <a href="https://leetcode.com/problems/lru-cache/description/">Leetcode LRU cache problem</a>
+ * using the java LinkedHashMap
+ * Very simple solution.
+ */
+public class LRUCacheV2 extends LinkedHashMap<Integer, Integer> {
+
+    private final int capacity;
+    public LRUCacheV2(int capacity) {
+        super(capacity, 0.75f, true); // true = access order
+        this.capacity = capacity;
+    }
+
+    int get(int key) {
+        Integer val = super.get(key);
+        if (val == null) {
+            return -1;
+        }
+
+        return val;
+    }
+
+    /*
+    * Evict oldest when size exceeds capacity
+    * */
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+
 }
