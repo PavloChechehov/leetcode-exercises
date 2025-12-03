140e6e16eedaeaf260259258535ac571463b3400
PavloChechehov <paulch1991@gmail.com>
2025-12-03 13:30:20 +0100

    feat(1) - Updated: Few implementation of the TaskScheduler

--- src/main/java/com/paul/atlassian/schedule/TaskScheduler.java
+++ src/main/java/com/paul/atlassian/schedule/TaskScheduler.java
@@ -1,7 +1,20 @@
 package com.paul.atlassian.schedule;
 
 import java.util.List;
+/*
+Day 4 — Priority Queues & Task Scheduling Basics
+Today's goal is to master:
+  -  PriorityQueue usage
+  -  Custom comparators
+  -  Designing a simple scheduling component
+  -  Handling insertions, cancellations, and executions
+  -  Clean code + clear reasoning (very important for Atlassian)
 
+
+Task 4.1 — Implement a Basic Task Scheduler (Core)
+
+You need to implement a scheduler with these functions:
+* */
 public interface TaskScheduler {
     void schedule(long time, Runnable task);
 
