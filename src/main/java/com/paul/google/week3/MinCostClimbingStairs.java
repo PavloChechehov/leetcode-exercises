a272ed2a727306e5960943535a665129cfb5410a
PavloChechehov <paulch1991@gmail.com>
2026-01-11 13:33:22 +0100

    feat(medium) - 746. Min Cost Climbing Stairs(dynamic programming)

--- src/main/java/com/paul/google/week3/MinCostClimbingStairs.java
+++ src/main/java/com/paul/google/week3/MinCostClimbingStairs.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class MinCostClimbingStairs {
 
     //dfs + memoization
-    public int minCostClimbingStairs(int[] cost) {
+/*    public int minCostClimbingStairs(int[] cost) {
         //     [1, 2, 1, 2, 1]
         //dp = [0, 0, 1, 2, 2, 3]
         // 10 15 20
@@ -35,5 +35,24 @@ public class MinCostClimbingStairs {
         memo.put(position, res);
 
         return res;
+    }*/
+
+    public int minCostClimbingStairs(int[] cost) {
+        //     [1, 2, 1, 2, 1]
+        //dp = [0, 0, 1, 2, 2, 3]
+        // 10 15 20
+        // 10
+        // dp[i] = Math.min(cost.get(i), cost.get(i + 1))
+
+        int n = cost.length;
+        int [] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i-1] + cost[i - 1], dp[i-2] + cost[i - 2]);
+        }
+
+        return dp[n];
     }
 }
