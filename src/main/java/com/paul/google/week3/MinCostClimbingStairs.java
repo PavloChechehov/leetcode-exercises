fdc20833d1e4ca3141587742c2fe0faa20c10261
PavloChechehov <paulch1991@gmail.com>
2026-01-11 13:32:44 +0100

    feat(medium) - 746. Min Cost Climbing Stairs

---
+++ src/main/java/com/paul/google/week3/MinCostClimbingStairs.java
@@ -0,0 +1,39 @@
+package com.paul.google.week3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/min-cost-climbing-stairs/description/">746. Min Cost Climbing Stairs</a>
+ */
+public class MinCostClimbingStairs {
+
+    //dfs + memoization
+    public int minCostClimbingStairs(int[] cost) {
+        //     [1, 2, 1, 2, 1]
+        //dp = [0, 0, 1, 2, 2, 3]
+        // 10 15 20
+        // 10
+        // dp[i] = Math.min(cost.get(i), cost.get(i + 1))
+
+        Map<Integer, Integer> memo = new HashMap<>();
+        int res = Math.min(dfs(cost, 0, 0, memo), dfs(cost, 1, 0, memo));
+        return res;
+    }
+
+    private int dfs(int[] cost, int position, int sum, Map<Integer, Integer> memo) {
+        if(memo.containsKey(position)) return memo.get(position);
+
+        if (position >= cost.length) {
+            return 0;
+        }
+
+        int res1 = dfs(cost, position + 1, sum, memo);
+        int res2 = dfs(cost, position + 2, sum, memo);
+        int res = cost[position] + Math.min(res1, res2);
+
+        memo.put(position, res);
+
+        return res;
+    }
+}
