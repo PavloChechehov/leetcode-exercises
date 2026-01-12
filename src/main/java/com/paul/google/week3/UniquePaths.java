dcd8aebb0f144d391a541923bb54c50fd370844e
PavloChechehov <paulch1991@gmail.com>
2026-01-12 15:50:42 +0100

    feat(medium) - 62. Unique Paths (dfs + memo)

--- src/main/java/com/paul/google/week3/UniquePaths.java
+++ src/main/java/com/paul/google/week3/UniquePaths.java
@@ -1,8 +1,6 @@
 package com.paul.google.week3;
 
 
-import java.util.Arrays;
-
 /**
  * <a href="https://leetcode.com/problems/unique-paths/description/">62. Unique Paths</a>
  */
@@ -13,8 +11,8 @@ public class UniquePaths {
         System.out.println(res);
     }
 
-    public static int uniquePaths(int m, int n) {
-        /* m = 3, n = 2
+    /*    public static int uniquePaths(int m, int n) {
+     *//* m = 3, n = 2
         1 1
         2 3
         2 3
@@ -23,7 +21,7 @@ public class UniquePaths {
         1 1
         1 0
         1 0
-        */
+        *//*
 
         int[][] dp = new int[m][n];
         Arrays.fill(dp[0], 1);
@@ -38,6 +36,47 @@ public class UniquePaths {
         }
 
         return dp[m - 1][n - 1];
+    }*/
+
+
+    static int[][] memo;
+    static int row;
+    static int col;
+
+    public static int uniquePaths(int m, int n) {
+        /* m = 3, n = 2
+        1 1
+        2 3
+        2 3
+
+        one steps is down one is left + 1
+
+        1 1
+        1 1
+        1 1
+        */
+
+        row = m;
+        col = n;
+        memo = new int[m][n];
+
+        return dfs(0, 0);
     }
+
+
+    private static int dfs(int i, int j) {
+        // Out of bounds
+        if (i >= row || j >= col) return 0;
+
+        // Reached destination
+        if (i == row - 1 && j == col - 1) return 1;
+
+        // Already computed
+        if (memo[i][j] != 0) return memo[i][j];
+
+        memo[i][j] = dfs(i + 1, j) + dfs(i, j + 1);
+        return memo[i][j];
+    }
+
 
 }
