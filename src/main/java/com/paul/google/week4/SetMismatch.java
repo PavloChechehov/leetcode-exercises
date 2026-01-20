6c4e2430c25a680f892a56afae7564809d286697
PavloChechehov <paulch1991@gmail.com>
2026-01-20 15:35:02 +0100

    feat(medium) - Leetcode Quest: Array: Q1. Set Mismatch

--- src/main/java/com/paul/google/week4/SetMismatch.java
+++ src/main/java/com/paul/google/week4/SetMismatch.java
@@ -16,14 +16,44 @@ public class SetMismatch {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 2, 4};
+        int[] arr = {2,2};
         int[] errorNums = findErrorNums(arr);
         System.out.println(Arrays.toString(errorNums));
 
     }
 
-
+    //my second solution without Set, only array
     public static int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+
+        int[] arr = new int[n + 1];
+
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int elem : nums) {
+            if (arr[elem] != 0) {
+                duplicate = elem;
+            } else {
+                arr[elem]++;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            int elem = arr[i];
+            if (elem == 0) {
+                missing = i;
+                break;
+            }
+        }
+        return new int[]{duplicate, missing};
+    }
+
+
+
+
+    /*public static int[] findErrorNums(int[] nums) {
         List<Integer> res = new ArrayList<>();
         int len = nums.length;
 
@@ -47,5 +77,5 @@ public class SetMismatch {
 
 
         return duplicates.stream().distinct().mapToInt(Integer::intValue).sorted().toArray();
-    }
+    }*/
 }
