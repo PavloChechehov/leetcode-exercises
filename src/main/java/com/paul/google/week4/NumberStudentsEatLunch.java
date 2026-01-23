c5d87a5ddbd225c026bce5c4682c88bbd32679cf
PavloChechehov <paulch1991@gmail.com>
2026-01-23 14:33:32 +0100

    feat(medium) - Leetcode Quest: Queue: Number of Students Unable to Eat Lunch

--- src/main/java/com/paul/google/week4/NumberStudentsEatLunch.java
+++ src/main/java/com/paul/google/week4/NumberStudentsEatLunch.java
@@ -53,4 +53,16 @@ public class NumberStudentsEatLunch {
 
         return studentQueue.size();
     }
+
+    //chat gpt solution
+    /*public int countStudents(int[] students, int[] sandwiches) {
+        int[] count = new int[2];
+        for (int s : students) count[s]++;
+
+        for (int sw : sandwiches) {
+            if (count[sw] == 0) break;
+            count[sw]--;
+        }
+        return count[0] + count[1];
+    }*/
 }
