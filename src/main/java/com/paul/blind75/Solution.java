c3cf0ead91a0aef0847c2b2fba382024f5d0982a
PavloChechehov <paulch1991@gmail.com>
2026-03-04 19:57:02 +0100

    feat(easy) - 94. Binary Tree Inorder Traversal

---
+++ src/main/java/com/paul/blind75/Solution.java
@@ -0,0 +1,32 @@
+package com.paul.blind75;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * <a href="https://leetcode.com/problems/binary-tree-inorder-traversal/description/">94. Binary Tree Inorder Traversal</a>
+ */
+class Solution {
+
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // 1 -> 2 -> 3
+        //createLeft(root.left), root.val, creartRight(root.right);
+
+        if (root == null) return List.of();
+
+        List<Integer> leftPart = inorderTraversal(root.left);
+        List<Integer> rightPart = inorderTraversal(root.right);
+
+        List<Integer> res = new ArrayList<>(leftPart);
+
+        res.add(root.val);
+        res.addAll(rightPart);
+
+        return res;
+    }
+
+}
