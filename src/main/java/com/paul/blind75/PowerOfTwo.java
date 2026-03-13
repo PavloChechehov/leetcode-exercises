f41ac08a8e5def43bfdce804f44dd86715f1a8d0
PavloChechehov <paulch1991@gmail.com>
2026-03-13 15:24:24 +0100

    feat(easy) - 231. Power of Two. One more solution

--- src/main/java/com/paul/blind75/PowerOfTwo.java
+++ src/main/java/com/paul/blind75/PowerOfTwo.java
@@ -7,25 +7,46 @@ package com.paul.blind75;
 public class PowerOfTwo {
 
 
-        public boolean isPowerOfTwo(int n) {
-            if (n < 0) return false;
-            if (n == 0) return false;
+    //solution1
+    public boolean isPowerOfTwo(int n) {
+        if (n < 0) return false;
+        if (n == 0) return false;
 
-            int pow = 0;
-            //16 - 8 - 4 - 2 - 1
+        int pow = 0;
+        //16 - 8 - 4 - 2 - 1
 
-            int curr = n;
+        int curr = n;
 
-            while(curr != 1) {
-                curr /= 2;
-                pow++;
-            }
-
-
-            for(int i = 0; i < pow; i++){
-                curr *= 2;
-            }
-
-            return n == curr;
+        while (curr != 1) {
+            curr /= 2;
+            pow++;
         }
+
+
+        for (int i = 0; i < pow; i++) {
+            curr *= 2;
+        }
+
+        return n == curr;
+    }
+
+    //solution2: with shiftbits
+/*    public boolean isPowerOfTwo(int n) {
+        if (n < 0) return false;
+        if (n == 0) return false;
+
+        int pow = 0;
+        //16 - 8 - 4 - 2 - 1
+
+        int curr = n;
+        int temp = 1;
+
+        while (curr != 1) {
+            curr = curr >> 1;
+            temp = temp << 1;
+        }
+
+        return n == temp;
+    }*/
+
 }
