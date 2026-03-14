c1dc62010d8af99533b7a3cce6e990f44f7f2281
PavloChechehov <paulch1991@gmail.com>
2026-03-14 18:00:06 +0100

    feat(medium) - 1415. The k-th Lexicographical String of All Happy Strings of Length n

---
+++ src/main/java/com/paul/blind75/KthLexicographicalString.java
@@ -0,0 +1,40 @@
+package com.paul.blind75;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * <a href="https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/">1415. The k-th Lexicographical String of All Happy Strings of Length n</a>
+ */
+public class KthLexicographicalString {
+
+    private final char[] chars = {'a', 'b', 'c'};
+
+    public String getHappyString(int n, int k) {
+        List<String> list = new ArrayList<>();
+        build(n, "", list);
+
+        return list.size() >= k ? list.get(k - 1) : "";
+    }
+
+
+    //dfs
+    private void build(int n, String s, List<String> list) {
+        if (n == 0) {
+            list.add(s);
+            return;
+        }
+
+        for (char c : chars) {
+            if (s.isEmpty()) {
+                build(n - 1, String.valueOf(c), list);
+
+            } else if (s.charAt(s.length() - 1) != c) {
+                build(n - 1, s + c, list);
+            }
+        }
+
+        return;
+    }
+}
