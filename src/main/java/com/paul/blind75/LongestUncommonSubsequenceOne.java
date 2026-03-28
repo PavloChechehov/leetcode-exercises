6efe17c6486d315645f623a1f75877df4f13a05a
PavloChechehov <paulch1991@gmail.com>
2026-03-28 16:37:33 +0100

    feat(easy) - 521. Longest Uncommon Subsequence I. Idk to say about this task, disappointed

--- src/main/java/com/paul/blind75/LongestUncommonSubsequenceOne.java
+++ src/main/java/com/paul/blind75/LongestUncommonSubsequenceOne.java
@@ -4,33 +4,21 @@ package com.paul.blind75;
  * <a href="https://leetcode.com/problems/longest-uncommon-subsequence-i/description/">521. Longest Uncommon Subsequence I</a>
  */
 public class LongestUncommonSubsequenceOne {
-    public int findLUSlength(String a, String b) {
-        if (a.equals(b)) return -1;
 
-        int la = a.length();
-        int lb = b.length();
-        int p1 = 0;
-        int p2 = 0;
-        int count = 0;
+    public static void main(String[] args) {
+        String a = "aaa";
+        String b = "aaa";
 
-        while(p1 < la && p2 < lb) {
-            if(a.charAt(p1) == b.charAt(p2)) {
-                count++;
-            }
+        int res = findLUSlength(a, b);
+        System.out.println(res);
+    }
 
-            p1++;
-            p2++;
-        }
-
-
-
-        int min = Math.min(a.length(), b.length());
-
-        if (count <= min) {
-            return Math.max(a.length(), b.length());
-        } else {
+    // this is a solution,
+    public static int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
             return -1;
+        } else {
+            return Math.max(a.length(), b.length());
         }
-
     }
 }
